3e5a84ee31debcb1361505f41083fe43a4218b0f
rnawslgk <hamo35301@gmail.com>
2022-08-09 00:41:57 +0900

    init+sign 로그인/회원가입 1차 작업

--- app/src/main/java/com/example/carrotmarket_cloneproject/Activity_chat.java
+++ app/src/main/java/com/example/carrotmarket_cloneproject/Activity_chat.java
@@ -9,5 +9,6 @@ public class Activity_chat extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.chat_layout);
     }
 }
